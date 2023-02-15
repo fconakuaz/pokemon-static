dd02f11ca3e3b4c4eaeeda0c724101cf6b7573e2
Francisco Acosta <francisco.acosta@softtek.com>
2023-02-15 16:23:15 -0600

    Configuración NextUI themeDark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import { darkTheme } from 'themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   // 2. Use at the root of your app
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={ darkTheme }>
       <Component {...pageProps} />
     </NextUIProvider>
   )
