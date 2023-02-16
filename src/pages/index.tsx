4c896a0234925d143ea94ef146c63bcd74fe8016
Francisco Acosta <francisco.acosta@softtek.com>
2023-02-16 14:26:27 -0600

    Se agregan cards sin componente aislado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,45 @@
+import { Card, grayDark, Grid, Row, Text } from "@nextui-org/react"
 import { pokeApi } from "api"
 import { Layout } from "components/layouts"
+import { PokemonListResponse, SmallPokemon } from "interfaces"
 import { NextPage, GetStaticProps } from "next"
  
-const HomePage: NextPage = (props) => {
-  console.log('props')
-  console.log(props)
+interface Props {
+  pokemons: SmallPokemon[]
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  console.log('✅✅✅✅✅✅   props   ✅✅✅✅✅')
+  console.log(pokemons)
   return (
     <Layout title={'Pokedex'} >
-      <ul>
-        <li>Pokemón</li>
-        <li>Pokemón</li>
-        <li>Pokemón</li>
-        <li>Pokemón</li>
-        <li>Pokemón</li>
-      </ul>
+        <Grid.Container gap={2} justify='flex-start'>
+          {pokemons?.map(p => {
+            let primeiraLetra = p.name.charAt(0).toUpperCase();
+            let nameCapital = primeiraLetra + p.name.slice(1);
+
+            return <Grid xs={6} sm={3} xl={2} key={p.id}>
+              <Card isPressable>
+                <Card.Body css={{ p: 12, backgroundColor:'#131313' }}>
+                  <Card.Image
+                    src={p.img}
+                    objectFit="cover"
+                    height={140}
+                    alt={p.name}
+                  />
+                </Card.Body>
+                <Card.Footer css={{ justifyItems: "flex-start" }}>
+                  <Row wrap="wrap" justify="space-between" align="center">
+                    <Text b>{nameCapital}</Text>
+                    <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                      #{p.id}
+                    </Text>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
+        })}
+        </Grid.Container> 
     </Layout>
   )
 }
@@ -22,19 +48,19 @@ const HomePage: NextPage = (props) => {
 //- The data required to render the page is available at build time ahead of a user’s request.
 //- The data comes from a headless CMS.
 //- The data can be publicly cached (not user-specific).
-//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
- 
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance. 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  // const { data } = await  // your fetch function here 
-  //  https://pokeapi.co/api/v2/pokemon?limit=151
-  
-  const {data} = await pokeApi.get('pokemon?limit=151')
-  console.log('✅✅✅ Holaaaa getStaticProps ✅✅✅',data)
-  
-  return {
-    props: {
-      pokemons: data?.results
+  const {data} = await pokeApi.get<PokemonListResponse>('pokemon?limit=151')  
+  const pokemons: SmallPokemon[] = data?.results?.map(pokemon => {
+    const idPokemon = pokemon?.url.split('/')[6] || 0 
+    return {
+      ...pokemon,
+      id:   Number(idPokemon),
+      img:  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idPokemon}.svg`
     }
+  })  
+  return {
+    props: { pokemons }
   }
 }
 
