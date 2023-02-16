f810d8b07f98bee0534c653b40143b325810caf7
Francisco Acosta <francisco.acosta@softtek.com>
2023-02-16 11:05:55 -0600

    Se agrega carga de pokemons desde la API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import { pokeApi } from "api"
 import { Layout } from "components/layouts"
-import { NextPage } from "next"
+import { NextPage, GetStaticProps } from "next"
  
 const HomePage: NextPage = (props) => {
   console.log('props')
@@ -16,4 +17,25 @@ const HomePage: NextPage = (props) => {
     </Layout>
   )
 }
+ 
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+ 
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // const { data } = await  // your fetch function here 
+  //  https://pokeapi.co/api/v2/pokemon?limit=151
+  
+  const {data} = await pokeApi.get('pokemon?limit=151')
+  console.log('✅✅✅ Holaaaa getStaticProps ✅✅✅',data)
+  
+  return {
+    props: {
+      pokemons: data?.results
+    }
+  }
+}
+
 export default HomePage
